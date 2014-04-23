2f4718baa74622dea0a5c8a3d3c23b8134dfd45e
Rushikesh Agashe <ragashe@ncsu.edu>
2014-04-23 19:37:29 -0400

    Added functionality to run sunspec_client.py as standalone script or as an imported module. Reorganized Database Structure.

--- sunspec_client.py
+++ sunspec_client.py
@@ -23,10 +23,13 @@ def create_table(table_name, point_type_dict):
 	table_columns_and_type_str = str(tuple(table_columns_and_type_str)).replace("'","")
 	
 	conn = sqlite3.connect('../sunspec_database/BEAGLEBONE_BLACK_1.db')
+	#conn = sqlite3.connect('../sunspec_database/sunspec_'+db_timestamp+'_BEAGLEBONE_BLACK_1.db')
 	c = conn.cursor()
-	timestamp =  datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 	c.execute("CREATE TABLE IF NOT EXISTS "+table_name+table_columns_and_type_str)
-	conn.commit()
+	try:
+		conn.commit()
+	except:
+		pass
 	c.close()
 	conn.close()
 
@@ -40,6 +43,7 @@ def data_entry(table_name, point_value_dict):
 	data = [item[1] for item in point_value_list]
 	data = tuple(data)
 	conn = sqlite3.connect('../sunspec_database/BEAGLEBONE_BLACK_1.db')
+	#conn = sqlite3.connect('../sunspec_database/sunspec_'+db_timestamp+'_BEAGLEBONE_BLACK_1.db')
 	c = conn.cursor()
 	sql_table_values_str = []
 	for i in range(0,len(table_columns_str.split(','))):
@@ -55,8 +59,84 @@ def clear_screen():
         	os.system("cls")
     	else:
         	os.system("clear")
+
+def stop():
+	global runFlag
+	runFlag = False
+	while runFlag == False:
+		pass
 	
-def main():
+def run(timestamp,port, protocol='RTU', slave_id='1', baudrate='9600'):
+	global runFlag
+	global db_timestamp
+	db_timestamp = timestamp
+	d = client.SunSpecClientDevice(client.RTU,slave_id=slave_id,name=port,baudrate=baudrate)
+	d.read()
+	model_ids = d.device.models.keys()
+	dirpath = '../pysunspec-clone/sunspec/models/smdx/'
+	reader_blocks = {}
+	point_attr_dict = OrderedDict()
+	point_attr_dict ['timestamp'] 	= 'TIMESTAMP'
+	point_attr_dict ['id'] 		= 'TEXT'
+	point_attr_dict ['base_value'] 	= 'TEXT'
+	point_attr_dict ['sf'] 		= 'TEXT'
+	point_attr_dict ['units'] 	= 'TEXT'
+	point_attr_dict ['type'] 	= 'TEXT'
+	point_attr_dict ['access'] 	= 'TEXT'
+		
+	for model_id in model_ids:
+		reader_blocks[str(model_id)] = ModelReader()
+		rb = reader_blocks[str(model_id)]	
+		rb.input_filepath = dirpath + 'smdx_' + '{0:05}'.format(model_id) + '.xml'
+		rb.output_filepath = 'smdx_data_' + '{0:05}'.format(model_id) + '.xml' 
+		xmldoc = minidom.parse(rb.input_filepath)
+		points = xmldoc.getElementsByTagName("model")[0].getElementsByTagName("point")
+		table_name = 'smdx_data_' + '{0:05}'.format(model_id)
+		create_table(table_name,point_attr_dict)
+
+	runFlag = True
+	while runFlag == True:
+		d.read()
+		model_ids = d.device.models.keys()
+		if __name__ == "__main__":
+			clear_screen()
+		itr = 0
+		timestamp = datetime.now()
+		for model_id in model_ids:
+			rb = reader_blocks[str(model_id)]
+			xmldoc = minidom.parse(rb.input_filepath)
+			table_name = 'smdx_data_' + '{0:05}'.format(model_id)
+			points = xmldoc.getElementsByTagName("model")[0].getElementsByTagName("point")
+			data_points = d.device.models.values()[itr][0].points
+			for point in points:
+				point_attr_dict = OrderedDict()
+				point_attr_dict['timestamp'] = timestamp
+				point_attr_dict['id'] = point.attributes['id'].value.encode('ascii')
+				point_attr_dict['type'] = point.attributes['type'].value.encode('ascii')
+				if point_attr_dict['type'] not in ['pad','sunssf']:
+					point_attr_dict['base_value'] = data_points[point_attr_dict['id']].value_base
+				try:
+					point_attr_dict['sf'] = data_points[point_attr_dict['id']].value_sf
+				except KeyError:
+					pass
+				try:
+					point_attr_dict['units'] = point.attributes['units'].value.encode('ascii')
+				except KeyError:
+					pass
+				try:
+					point_attr_dict['access'] = point.attributes['access'].value.encode('ascii')
+				except KeyError:
+					point_attr_dict['access'] = 'r'
+				data_entry(table_name,point_attr_dict)
+			itr = itr + 1
+		if __name__ == "__main__" or DEBUG==True:
+			print d
+	d.close()
+	return
+runFlag = True
+DEBUG = False
+db_timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='SunSpec MODBUS Polling Client')
 	parser.add_argument('--port',required=True, help='Serial COM Port')
 	parser.add_argument('--protocol', nargs='?', default='RTU', help='Protocol for MODBUS communication')
@@ -68,73 +148,4 @@ def main():
 	    protocol = client.TCP
 	else:
 	    protocol = client.RTU
-	print args
-
-	d = client.SunSpecClientDevice(client.RTU,slave_id=args.slave_id,name=args.port,baudrate=args.baudrate)
-	d.read()
-	model_ids = d.device.models.keys()
-	dirpath = 'sunspec/models/smdx/'
-	reader_blocks = {}
-	point_type_list = []
-	for model_id in model_ids:
-		point_type_dict = OrderedDict()
-		point_type_dict['data_timestamp'] = 'TIMESTAMP'
-		reader_blocks[str(model_id)] = ModelReader()
-		rb = reader_blocks[str(model_id)]	
-		rb.input_filepath = dirpath + 'smdx_' + '{0:05}'.format(model_id) + '.xml'
-		rb.output_filepath = 'smdx_data_' + '{0:05}'.format(model_id) + '.xml' 
-		xmldoc = minidom.parse(rb.input_filepath)
-		points = xmldoc.getElementsByTagName("model")[0].getElementsByTagName("point")
-		for point in points:
-			key = point.attributes['id'].value.encode('ascii')
-			value = point.attributes['type'].value.encode('ascii')
-			point_type_dict[key] = value
-			if value  == 'string':
-				point_type_dict[key] = 'TEXT'
-			elif value in ['uint16', 'int16', 'uint32', 'int32']:
-				point_type_dict[key] = 'REAL'
-			elif value in ['sunssf','pad']:
-				del point_type_dict[key]
-				continue
-			point_type_list.append([key ,point_type_dict[key]])	
-		table_name = 'smdx_data_' + '{0:05}'.format(model_id)
-		create_table(table_name,point_type_dict)
-
-	while True:
-		d.read()
-		model_ids = d.device.models.keys()
-		clear_screen()
-		itr = 0
-		timestamp = datetime.now()
-		for model_id in model_ids:
-			point_value_dict = OrderedDict()
-			point_value_dict['data_timestamp'] = timestamp
-			rb = reader_blocks[str(model_id)]
-			xmldoc = minidom.parse(rb.input_filepath)
-			points = xmldoc.getElementsByTagName("model")[0].getElementsByTagName("point")
-			data_points = d.device.models.values()[itr][0].points
-			for point in points:
-				point_type =  point.attributes['type'].value.encode('ascii')
-				point_id = point.attributes['id'].value.encode('ascii')
-				if point_type in ['pad','sunssf']:
-					point.appendChild(xmldoc.createTextNode(''))
-				else:
-					if data_points[point_id].value_sf is not None:
-						scaling_factor = 10**data_points[point_id].value_sf
-						point_value = scaling_factor*data_points[point_id].value_base
-					else:
-						point_value = data_points[point_id].value_base
-					
-					point_value = '--' if point_value is None else point_value
-					point_value_dict[point_id] = point_value
-			#		point.appendChild(xmldoc.createTextNode(str(point_value)))
-			#output_xmlfile = open(rb.output_filepath, "w+")
-			#xmldoc.writexml(output_xmlfile)
-			#output_xmlfile.close()
-			itr = itr + 1
-			table_name = 'smdx_data_' + '{0:05}'.format(model_id)
-			data_entry(table_name,point_value_dict)
-		print d
-	    
-if __name__ == "__main__":
-	main()
+	run(timestamp=db_timestamp,port=args.port, protocol=args.protocol, slave_id=args.slave_id, baudrate=args.baudrate)
